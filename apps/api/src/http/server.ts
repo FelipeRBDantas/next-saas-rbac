a843371489fd15b917cc23eddc50a390a813e4eb
feliperbdantas <feliperbdantas@outlook.com>
2025-08-19 20:49:59 -0300

    feat: 2 - project setup and permissions - password change route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -14,6 +14,7 @@ import { errorHandler } from './error-handler'
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
+import { requestPasswordRecover } from './routes/auth/request-password-recover'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -49,6 +50,8 @@ app.register(createAccount)
 app.register(authenticateWithPassword)
 
 app.register(getProfile)
+
+app.register(requestPasswordRecover)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP server running on http://localhost:3333')
