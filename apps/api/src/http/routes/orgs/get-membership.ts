dd7c8ba4f87b51c6385a3a92088a68b6eea6513e
feliperbdantas <feliperbdantas@outlook.com>
2025-08-31 23:31:28 -0300

    feat: 2 - project setup and permissions - obtaining affiliation

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,45 @@
+import { roleSchema } from '@saas/auth/src/roles'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+
+export async function getMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/membership',
+      {
+        schema: {
+          tags: ['Organizations'],
+          summary: 'Get authenticated user membership of an organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({ slug: z.string() }),
+          response: {
+            200: z.object({
+              membership: z.object({
+                id: z.string().uuid(),
+                role: roleSchema,
+                organizationId: z.string().uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
+
+        const { membership } = await request.getUserMembership(slug)
+
+        return {
+          membership: {
+            id: membership.id,
+            role: roleSchema.parse(membership.role),
+            organizationId: membership.organizationId,
+          },
+        }
+      },
+    )
+}
