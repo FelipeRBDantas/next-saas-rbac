28062270c4a8248aafc3768251410ce38fbb1a21
feliperbdantas <feliperbdantas@outlook.com>
2025-09-04 19:05:02 -0300

    feat: 2 - project setup and permissions - route transfer organization

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -23,6 +23,7 @@ import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization.ts'
+import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -86,6 +87,8 @@ app.register(getOrganizations)
 app.register(updateOrganization)
 
 app.register(shutdownOrganization)
+
+app.register(transferOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('🚀 HTTP server running on http://localhost:3333')
