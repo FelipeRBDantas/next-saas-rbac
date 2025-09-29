d65e06cba0f8f849ffae6b22b15ba16370044e1e
feliperbdantas <feliperbdantas@outlook.com>
2025-09-29 19:10:37 -0300

    feat: frontend - authentication page

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Create Next App',
+  title: 'Next SaaS App',
 }
 
 export default function RootLayout({
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body>{children}</body>
+    <html lang="pt-BR" className="dark">
+      <body suppressHydrationWarning>{children}</body>
     </html>
   )
 }
