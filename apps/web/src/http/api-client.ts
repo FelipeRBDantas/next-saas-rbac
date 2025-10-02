3e750538d8a5899b2fa8829b1fa3bb08a680303e
feliperbdantas <feliperbdantas@outlook.com>
2025-10-02 19:47:46 -0300

    chore: 3 - developing the frontend - http client setup - change ky for 1.7.0

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,6 +1,10 @@
-import ky from 'ky'
+import ky, { type Input } from 'ky'
 
 export const api = ky.create({
   prefixUrl: 'http://localhost:3333',
-  fetch: (...args) => fetch(...args),
+  fetch: (input: Input, init?: RequestInit) => {
+    const normalizedInput = input instanceof URL ? input.toString() : input
+
+    return fetch(normalizedInput, init)
+  },
 })
