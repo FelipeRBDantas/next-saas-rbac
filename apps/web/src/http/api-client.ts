e8317703a6a818bc8506df88ba7a87637e1b2566
feliperbdantas <feliperbdantas@outlook.com>
2025-10-08 20:13:23 -0300

    feat: 3 - developing the frontend - getting authenticated user

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,7 +1,26 @@
+import type { CookiesFn } from 'cookies-next'
+import { getCookie } from 'cookies-next'
 import ky, { type Input } from 'ky'
 
 export const api = ky.create({
   prefixUrl: 'http://localhost:3333',
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let cookieStore: CookiesFn | undefined
+
+        if (typeof window === 'undefined') {
+          const { cookies: serverCookies } = await import('next/headers')
+
+          cookieStore = serverCookies
+        }
+
+        const token = getCookie('token', { cookies: cookieStore })
+
+        if (token) request.headers.set('Authorization', `Bearer ${token}`)
+      },
+    ],
+  },
   fetch: (input: Input, init?: RequestInit) => {
     const normalizedInput = input instanceof URL ? input.toString() : input
 
