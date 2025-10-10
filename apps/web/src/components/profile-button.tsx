cf1b48dd34c34200a37b080b73d38b573bdce9b5
feliperbdantas <feliperbdantas@outlook.com>
2025-10-10 18:29:58 -0300

    feat: 3 - developing the frontend - header component

--- apps/web/src/components/profile-button.tsx
+++ apps/web/src/components/profile-button.tsx
@@ -1,6 +1,6 @@
 import { ChevronDown, LogOut } from 'lucide-react'
-import Link from 'next/link'
 
+import { auth } from '@/auth/auth'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import {
   DropdownMenu,
@@ -9,24 +9,38 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 
-export function ProfileButton() {
+function getInitials(name: string): string {
+  const initials = name
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase())
+    .slice(0, 2)
+    .join('')
+
+  return initials
+}
+
+export async function ProfileButton() {
+  const { user } = await auth()
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex items-center gap-3 outline-none cursor-pointer">
         <div className="flex flex-col items-end">
-          <span className="text-sm font-medium">Felipe Dantas</span>
-          <span className="text-xs text-muted-foreground">
-            feliperbdantas@Outlook.com
-          </span>
+          <span className="text-sm font-medium">{user?.name}</span>
+          <span className="text-xs text-muted-foreground">{user?.email}</span>
         </div>
 
         <Avatar>
-          <AvatarImage
-            src="https://github.com/feliperbdantas.png"
-            alt="Felipe Dantas"
-          />
+          {user?.avatarUrl && (
+            <AvatarImage
+              src={user?.avatarUrl}
+              alt={user?.name ?? 'Foto de perfil'}
+            />
+          )}
 
-          <AvatarFallback>FD</AvatarFallback>
+          {user?.name && (
+            <AvatarFallback>{getInitials(user?.name)}</AvatarFallback>
+          )}
         </Avatar>
 
         <ChevronDown className="size-4 text-muted-foreground" />
@@ -34,10 +48,10 @@ export function ProfileButton() {
 
       <DropdownMenuContent align="end">
         <DropdownMenuItem asChild>
-          <Link href="/api/auth/sign-out">
+          <a href="/api/auth/sign-out" className="cursor-pointer">
             <LogOut className="mr-2 size-4" />
             Sign out
-          </Link>
+          </a>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
