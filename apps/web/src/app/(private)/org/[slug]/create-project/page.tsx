9cc4ec2a20d6d369463e01af43928dea43c95147
feliperbdantas <feliperbdantas@outlook.com>
2025-11-05 19:15:29 -0300

    feat: 3 - developing the frontend - create project page

---
+++ apps/web/src/app/(private)/org/[slug]/create-project/page.tsx
@@ -0,0 +1,11 @@
+import { ProjectForm } from './project-form'
+
+export default function CreateProject() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Create project</h1>
+
+      <ProjectForm />
+    </div>
+  )
+}
